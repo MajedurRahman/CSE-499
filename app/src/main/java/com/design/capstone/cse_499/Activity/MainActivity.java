1386322ca17feb5a436b06907f9ef0312f040aa1
Majedur Rahman <Majedur.Rahman.mmr@gmail.com>
2017-08-01 11:50:35 +0600

    Initial commit

---
+++ app/src/main/java/com/design/capstone/cse_499/Activity/MainActivity.java
@@ -0,0 +1,123 @@
+package com.design.capstone.cse_499.Activity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.design.capstone.cse_499.R;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+public class MainActivity extends AppCompatActivity {
+
+    TextView registerTv;
+    EditText emailTv, passwordTv;
+    Button loginBtn;
+    private FirebaseAuth mAuth;
+    private FirebaseAuth.AuthStateListener mAuthListener;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        mAuth = FirebaseAuth.getInstance();
+
+        initComponents();
+        onclickAction();
+        userAuth();
+
+
+    }
+
+    private void onclickAction() {
+
+        registerTv.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                startActivity(new Intent(MainActivity.this, RegistrationActivity.class));
+            }
+        });
+
+
+        loginBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String email =emailTv.getText().toString().trim();
+                String pass = passwordTv.getText().toString();
+               checkLogin(email,pass);
+            }
+        });
+
+    }
+
+    private void checkLogin(String email , String password) {
+
+        mAuth.signInWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                         // If sign in fails, display a message to the user. If sign in succeeds
+                        // the auth state listener will be notified and logic to handle the
+                        // signed in user can be handled in the listener.
+                        if (task.isSuccessful()) {
+
+                            Toast.makeText(MainActivity.this, "Login Successful", Toast.LENGTH_SHORT).show();
+                        }
+
+
+                    }
+                });
+    }
+
+    public void userAuth(){
+
+        mAuthListener = new FirebaseAuth.AuthStateListener() {
+            @Override
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+                FirebaseUser user = firebaseAuth.getCurrentUser();
+                if (user != null) {
+                    // User is signed in
+
+                } else {
+                    // User is signed out
+
+                }
+                // ...
+            }
+        };
+
+    }
+
+    private void initComponents() {
+
+
+        registerTv = (TextView) findViewById(R.id.registerTv);
+        loginBtn = (Button) findViewById(R.id.loginBtn);
+        emailTv = (EditText) findViewById(R.id.emailEt);
+        passwordTv = (EditText) findViewById(R.id.passwordEt);
+
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        mAuth.addAuthStateListener(mAuthListener);
+    }
+    @Override
+    public void onStop() {
+        super.onStop();
+        if (mAuthListener != null) {
+            mAuth.removeAuthStateListener(mAuthListener);
+        }
+    }
+}
---
+++ app/src/main/java/com/design/capstone/cse_499/Activity/RegistrationActivity.java
@@ -0,0 +1,192 @@
+package com.design.capstone.cse_499.Activity;
+
+import android.content.Intent;
+import android.support.annotation.NonNull;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.design.capstone.cse_499.R;
+import com.design.capstone.cse_499.Model.User;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+public class RegistrationActivity extends AppCompatActivity {
+
+    EditText fullNameEt , userNameEt , passwordEt, emailEt,phoneEt;
+    Button cancelBtn , signUpBtn;
+
+    String fullname, username , password,email,phonenumber;
+
+
+    // Write a message to the database
+    FirebaseDatabase database = FirebaseDatabase.getInstance();
+    DatabaseReference userRef = database.getReference("UserData");
+    private FirebaseAuth mAuth;
+    private FirebaseAuth.AuthStateListener mAuthListener;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_registration);
+        mAuth = FirebaseAuth.getInstance();
+
+        initComponent();
+        onClickAction();
+
+
+    }
+
+    private void onClickAction() {
+
+        cancelBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(RegistrationActivity.this, MainActivity.class));
+            }
+        });
+
+
+
+        signUpBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+               // Toast.makeText(RegistrationActivity.this, "Not Set Yet", Toast.LENGTH_SHORT).show();
+               if (checkSignUpData()){
+
+                   initUserObject();
+
+               }
+
+
+            }
+        });
+    }
+
+    private void initUserObject() {
+
+        User user = new User();
+
+        user.setFullName(fullname);
+        user.setEmail(email);
+        user.setUserName(username);
+        user.setPassword(password);
+        user.setPhoneNumber(phonenumber);
+
+        SendDatatoFireBase(user, phonenumber);
+        createUserAuth(user.getEmail(),user.getPassword());
+
+
+
+    }
+
+    public void createUserAuth(String email , String pass){
+
+
+        mAuth.createUserWithEmailAndPassword(email, pass)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        Log.d("Reg", "createUserWithEmail:onComplete:" + task.isSuccessful());
+
+                        // If sign in fails, display a message to the user. If sign in succeeds
+                        // the auth state listener will be notified and logic to handle the
+                        // signed in user can be handled in the listener.
+                        if (!task.isSuccessful()) {
+
+                            Toast.makeText(RegistrationActivity.this, " Registration Successful..", Toast.LENGTH_SHORT).show();
+                        }
+
+
+                    }
+                });
+    }
+
+    private void SendDatatoFireBase(User user , String id) {
+        userRef.child(id).setValue(user);
+
+    }
+
+    public boolean checkSignUpData(){
+
+        fullname = String.valueOf(fullNameEt.getText().toString().trim());
+        username = String.valueOf(userNameEt.getText().toString().trim());
+        password = passwordEt.getText().toString();
+        email = String.valueOf(emailEt.getText().toString().trim());
+        phonenumber = String.valueOf(phoneEt.getText().toString().trim());
+
+        if (fullname.isEmpty()){
+
+            Toast.makeText(this, "Full Name is Empty ", Toast.LENGTH_SHORT).show();
+        }
+        if (username.isEmpty()) {
+            Toast.makeText(this, "User Name is Empty ", Toast.LENGTH_SHORT).show();
+        }
+       if (password.isEmpty()){
+            Toast.makeText(this, "Password is Empty ", Toast.LENGTH_SHORT).show();
+
+        }
+      if (phonenumber.isEmpty()){
+            Toast.makeText(this, "Phone Number is Empty ", Toast.LENGTH_SHORT).show();
+
+        }
+        if (email.isEmpty()){
+
+            Toast.makeText(this, "Email is Empty ", Toast.LENGTH_SHORT).show();
+
+        }
+        if (!email.isEmpty()&& !phonenumber.isEmpty()&& !password.isEmpty()&& !username.isEmpty()&& !fullname.isEmpty()){
+
+            if (email.contains("@")&& email.contains(".")){
+
+                if (phonenumber.length()==11){
+
+                   if (phonenumber.startsWith("017")|| phonenumber.startsWith("018")||phonenumber.startsWith("016")
+                           || phonenumber.startsWith("019")|| phonenumber.startsWith("015")){
+
+                       return true;
+                   }
+                   else {
+
+                       Toast.makeText(this, "Invalid Phone Number", Toast.LENGTH_SHORT).show();
+                       return false;
+                   }
+                }
+                else {
+
+                    Toast.makeText(this, "Enter Correct Phone Number ", Toast.LENGTH_SHORT).show();
+                }
+            }
+            else {
+
+                Toast.makeText(this, "Please Enter Correct Email", Toast.LENGTH_SHORT).show();
+                return false;
+            }
+        }
+        return false;
+    }
+
+    public void  initComponent(){
+
+        cancelBtn = (Button) findViewById(R.id.cancelBtn);
+        signUpBtn = (Button) findViewById(R.id.signupBtn);
+
+        fullNameEt = (EditText) findViewById(R.id.fullNameEt);
+        userNameEt = (EditText) findViewById(R.id.userNameEt);
+        passwordEt = (EditText) findViewById(R.id.regPasswordEt);
+        emailEt  = (EditText) findViewById(R.id.emailEt);
+        phoneEt = (EditText) findViewById(R.id.phoneRt);
+
+    }
+
+
+}
